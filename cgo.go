46cf854b7b236308547482b9eb609d941ca67ca4
Sean Craswell <scraswell@gmail.com>
2025-04-24 12:53:09 -0300

    Suppressed warning.

--- cgo.go
+++ cgo.go
@@ -6,7 +6,7 @@ package v8go
 
 //go:generate clang-format -i --verbose -style=Chromium v8go.h v8go.cc
 
-// #cgo CXXFLAGS: -fno-rtti -fPIC -std=c++14 -DV8_COMPRESS_POINTERS -DV8_31BIT_SMIS_ON_64BIT_ARCH -I${SRCDIR}/deps/include -Wall
+// #cgo CXXFLAGS: -fno-rtti -fPIC -std=c++17 -DV8_COMPRESS_POINTERS -DV8_31BIT_SMIS_ON_64BIT_ARCH -I${SRCDIR}/deps/include -Wall -Wno-vla-cxx-extension
 // #cgo LDFLAGS: -pthread -lv8
 // #cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/deps/darwin_x86_64
 // #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/deps/darwin_arm64
@@ -18,9 +18,9 @@ import "C"
 // contain V8 libraries and headers which otherwise would be ignored.
 // DO NOT REMOVE
 import (
-	_ "rogchap.com/v8go/deps/darwin_arm64"
-	_ "rogchap.com/v8go/deps/darwin_x86_64"
-	_ "rogchap.com/v8go/deps/include"
-	_ "rogchap.com/v8go/deps/linux_arm64"
-	_ "rogchap.com/v8go/deps/linux_x86_64"
+	_ "github.com/customerio/v8go/deps/darwin_arm64"
+	_ "github.com/customerio/v8go/deps/darwin_x86_64"
+	_ "github.com/customerio/v8go/deps/include"
+	_ "github.com/customerio/v8go/deps/linux_arm64"
+	_ "github.com/customerio/v8go/deps/linux_x86_64"
 )
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module rogchap.com/v8go
+module github.com/customerio/v8go
 
 go 1.16
+
+require rogchap.com/v8go v0.9.0
